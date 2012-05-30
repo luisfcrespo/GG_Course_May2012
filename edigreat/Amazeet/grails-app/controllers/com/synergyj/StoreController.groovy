0f240bcf91afe278ce2e025d60af2d405f8fca2e
neodevelop <neodevelop@gmail.com>
2012-05-30 17:49:34 -0500

    Reformateando el código usando params.long para obtener el producto // Por eso no funcionaba

--- edigreat/Amazeet/grails-app/controllers/com/synergyj/StoreController.groovy
+++ edigreat/Amazeet/grails-app/controllers/com/synergyj/StoreController.groovy
@@ -22,89 +22,96 @@ class StoreController {
 
 	}
 
-    def index() {
-		//TODO: Catalogo de productos
-		def products = Product.list()
-		//Catalogo de categorias
-		//def categories = Category.list()
-		//Todas las acciones regresan un mapa
+  def index() {
+    //TODO: Catalogo de productos
+    def products = Product.list()
+    //Catalogo de categorias
+    //def categories = Category.list()
+    //Todas las acciones regresan un mapa
 
-		[products:products]
+    [products:products]
 
-		}
+  }
 
-	def reviewItem = {
-		//TODO: Obtener el id de producto y mostrar el detalle de los productos
-		//log.debug "Params ${params.id}"
-		redirect(controller:'product',action:'show',id:params.id)
+  def reviewItem = {
+    //TODO: Obtener el id de producto y mostrar el detalle de los productos
+    //log.debug "Params ${params.id}"
+    redirect(controller:'product',action:'show',id:params.id)
 
-		}
+  }
 
-	def addItem() {
-		def product = Product.get(params.productId)
-		def item = new ItemToPurchase(product:product,quantity:params.int('quantity'))
-		session.shoppingCart.items << item
-		log.debug item.dump()
+  def addItem() {
+    def product = Product.get(params.productId)
+    def item = new ItemToPurchase(product:product,quantity:params.int('quantity'))
+    session.shoppingCart.items << item
+    log.debug item.dump()
 
-		//Render puede recibir el objeto modelo  como un mapa y se lo pega a la vista
+    //Render puede recibir el objeto modelo  como un mapa y se lo pega a la vista
 
-		//log.debug request.dump()
-		//log.debug response.dump()
-//		log.debug session
-	//	log.debug servletContext.dump()
-	//	log.debug flash.dump()
-		flash.message = "Haz agregado un producto a tu carrito"
-		redirect action:'index'
+    //log.debug request.dump()
+    //log.debug response.dump()
+    //		log.debug session
+    //	log.debug servletContext.dump()
+    //	log.debug flash.dump()
+    flash.message = "Haz agregado un producto a tu carrito"
+    redirect action:'index'
 
-		}
-
-
-	def addItemAsync(){
-
-		def product = Product.get(params.productId)
-	    boolean isItem = false
-
-	    session.totalCompra = 0
-	    //Buscamos si el articulo ya se encuentra en shoppingCart, EqualsAndHashCode en Product
-		session.shoppingCart.items.each{ it ->
-
-                if(it.product == product)
-                {
-                  it.quantity =  it.quantity + params.int('quantity')
-                  isItem = true
-                }
-
-                session.totalCompra =session.totalCompra + it.product.price * it.quantity
-		}
-
-        //si no se encuentra lo agregamos
-        if(!isItem)
-	    {
-	    	def item = new ItemToPurchase(product:product,quantity:params.int('quantity'))
-            session.shoppingCart.items << item
-            session.totalCompra =session.totalCompra + item.product.price * item.quantity
-        }
-
-		render(template: "/store/myCart")
-
-		}
+  }
 
 
-      def showShoppingCart()
+  def addItemAsync(){
+    log.debug "Buscamos el product con el ID"
+    def product = Product.get(params.long('productId'))
+    log.debug "Iniciando "
+    boolean isItem = false
+    log.debug "Creando totalCompra en session"
+    session.totalCompra = 0
+    //Buscamos si el articulo ya se encuentra en shoppingCart, EqualsAndHashCode en Product
+    log.debug "Iteramos los items en shoppingCart"
+    session.shoppingCart.items.each{ it ->
+      log.debug "Comparamos el producto obtenido con el de la lista"
+      if(it.product == product)
       {
-         render(template: "/store/confirmCart")
+        log.debug "Son iguales, sumamos uno al qunatity del detalle"
+        it.quantity =  it.quantity + params.int('quantity')
+        log.debug "Si es un objeto"
+        isItem = true
       }
+      log.debug "Recalculamos el total de la compra"
+      session.totalCompra =session.totalCompra + it.product.price * it.quantity
+    }
 
-      def cancelShoppingCart()
-      {
-         session.shoppingCart=null
-		   redirect action:'index'
-      }
+    log.debug "si no se encuentra lo agregamos"
+    if(!isItem)
+    {
+      log.debug "creamos el item"
+      def item = new ItemToPurchase(product:product,quantity:params.int('quantity'))
+      log.debug "Lo agregamos a la lista de items en el shoppingCart"
+      session.shoppingCart.items << item
+      log.debug "Recalculamos el precio total"
+      session.totalCompra =session.totalCompra + item.product.price * item.quantity
+    }
+    log.debug "Rendereamos el template"
+    render(template: "/store/myCart")
+
+  }
 
 
-      def createOrder()
-      {
+  def showShoppingCart()
+  {
+   render(template: "/store/confirmCart")
+ }
 
-      }
+ def cancelShoppingCart()
+ {
+   session.shoppingCart=null
+   redirect action:'index'
+ }
+
+
+ def createOrder()
+ {
+
+ }
 }
 
