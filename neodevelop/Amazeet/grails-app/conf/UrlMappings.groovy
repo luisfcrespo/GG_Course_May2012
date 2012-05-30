3c8564dbaf81edcacbb61008f0df55fb6fe60734
neodevelop <neodevelop@gmail.com>
2012-05-30 15:25:29 -0500

    Agregando servicios REST a product

--- neodevelop/Amazeet/grails-app/conf/UrlMappings.groovy
+++ neodevelop/Amazeet/grails-app/conf/UrlMappings.groovy
@@ -12,6 +12,9 @@ class UrlMappings {
 		//"/"(view:"/index")
 		"/"(controller:"store",action:'index')
 		"/checkoutMyCart"(controller:"store",action:"checkout")
+		"/rest/$id?"(controller:'productRest',parseRequest:true){
+			action = [GET:'show',POST:'save',DELETE:'delete']
+		}
 		"500"(view:'/error')
 	}
 }
