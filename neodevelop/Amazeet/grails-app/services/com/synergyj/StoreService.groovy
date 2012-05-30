14406cf14920ad5394e0b23d1cf35af67c2489c8
neodevelop <neodevelop@gmail.com>
2012-05-30 00:42:45 -0500

    Agregando el método de negocio

--- neodevelop/Amazeet/grails-app/services/com/synergyj/StoreService.groovy
+++ neodevelop/Amazeet/grails-app/services/com/synergyj/StoreService.groovy
@@ -13,4 +13,23 @@ class StoreService {
     }
     log.debug "Estoy en el método del servicio"
   }
+
+  def saveShoppingCart(ShoppingCart shoppingCart){
+    def shoppingCartToSave = new ShoppingCart(shoppingCart.properties)
+    def totalAmount = 0
+    shoppingCart.items.each{ item ->
+      totalAmount += (item.quantity * item.product.price)
+      shoppingCartToSave.addToItems(item)
+    }
+    shoppingCartToSave.save()
+    def payment = new Payment(
+      amount:totalAmount,
+      commission:0,
+      description:"Pago generado",
+      paymentStatus:PaymentStatus.WAITING
+    )
+    def shoppingCartOrder = new ShoppingCartOrder(shoppingCartOrderStatus:ShoppingCartOrderStatus.PLACED,shoppingCart:shoppingCartToSave,payment:payment)
+    shoppingCartOrder.save()
+    payment
+  }
 }
